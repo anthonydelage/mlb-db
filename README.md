44ba7013a256f9fd2ebeed1a3fa1639678707927
Anthony Delage <anthony.delage@qubit.com>
2018-04-14 19:34:17 -0400

    Add historical player ID maps to  table.

--- README.md
+++ README.md
@@ -3,10 +3,11 @@ mlb-db
 
 Create and update a local MLB stats database.
 
-Gets data from two main sources:
+Gets data from three main sources:
 
   - Baseball Savant (Statcast): uses the Statcast Search tool to collect pitch-by-pitch logs for every team and player. Sample download [here](https://baseballsavant.mlb.com/statcast_search/csv?all=true&hfPT=&hfAB=&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=&hfC=&hfSea=2018%7C&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=&game_date_lt=&team=NYY&position=&hfRO=&home_road=&hfFlag=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name-event&sort_col=pitches&player_event_sort=api_p_release_speed&sort_order=desc&min_abs=0&type=details&).
-  - Crunchtime Baseball player maps: a full table of MLB players by MLBAM ID, mapped to their IDs in other "systems". Sample download [here](http://crunchtimebaseball.com/master.csv).
+  - Crunchtime Baseball player maps: a full table of current MLB players by MLBAM ID, mapped to their IDs in other "systems". Sample download [here](http://crunchtimebaseball.com/master.csv).
+  - Baseball Prospectus player maps: a table containing current and retired MLB players. Not as complete as the Crunchtime Baseball maps. Sample download [here](http://www.baseballprospectus.com/sortable/playerids/playerid_list.csv).
 
 Requirements
 ----------
--- src/update.py
+++ src/update.py
@@ -10,6 +10,7 @@ Arguments:
   --year (int): A year to update for the `statcast` table. Defaults to the current year.
   --no-statcast: Skips updating the `statcast` table.
   --no-players: Skips updating the `players` table.
+  --historical-players: Enables historical player updates (backfills old IDs).
 """
 
 import os
@@ -34,6 +35,7 @@ TEAMS = ['ARI', 'ATL', 'BAL', 'BOS', 'CHC',
          'PHI', 'PIT', 'SD', 'SEA', 'SF',
          'STL', 'TB', 'TEX', 'TOR', 'WSH']
 
+
 def update_statcast(engine, year):
   """Update statcase table in database
 
@@ -57,11 +59,28 @@ def update_statcast(engine, year):
     print('UPDATING {team}, {year} AT {time}'.format(**params))
 
     uri = base_uri.format(**params)
-    data = _get_data(uri)
+    dtypes = {
+      'sv_id': 'str',
+      'batter': 'str',
+      'pitcher': 'str',
+      'game_pk': 'str',
+      'umpire': 'str',
+      'pos1_person_id': 'str',
+      'pos2_person_id': 'str',
+      'pos3_person_id': 'str',
+      'pos4_person_id': 'str',
+      'pos5_person_id': 'str',
+      'pos6_person_id': 'str',
+      'pos7_person_id': 'str',
+      'pos8_person_id': 'str',
+      'pos9_person_id': 'str'
+    }
+    data = _get_data(uri, dtypes=dtypes)
 
     if not data.empty:
       data = _prep_statcast(data)
-      _to_db_with_temp(data, engine, table_name=DB_CONFIG['statcast_table'])
+      _to_db_with_temp(data=data, engine=engine, table_name=DB_CONFIG['statcast_table'])
+
 
 def update_players(engine):
   """Update players table in database
@@ -74,12 +93,49 @@ def update_players(engine):
   """
 
   uri = API_CONFIG['players']
-  data = _get_data(uri, encoding='ISO-8859-1')
+  dtypes = {
+    'mlb_id': 'str',
+    'bp_id': 'str',
+    'bref_id': 'str',
+    'cbs_id': 'str',
+    'espn_id': 'str',
+    'fg_id': 'str',
+    'lahman_id': 'str',
+    'nfbc_id': 'str',
+    'retro_id': 'str',
+    'yahoo_id': 'str',
+    'ottoneu_id': 'str',
+    'rotowire_id': 'str'
+  }
+  data = _get_data(uri, dtypes=dtypes, encoding='ISO-8859-1')
 
   if not data.empty:
-    _to_db_with_temp(data, engine, table_name=DB_CONFIG['players_table'])
+    _to_db_with_temp(data=data, engine=engine, table_name=DB_CONFIG['players_table'])
 
-def _get_data(uri, max_tries=10, encoding='utf-8'):
+
+def update_players_historical(engine):
+  """Update players table in database
+
+  Queries the Crunchtime Baseball CSV API for every player in MLB,
+    then inserts the data in the MySQL `mlbdb` database's `players` table. 
+
+  Args:
+    engine (engine): A SQLAlchemy engine connected to the database to update.
+  """
+
+  uri = API_CONFIG['players_historical']
+  dtypes = {
+    'MLBCODE': 'str',
+    'RETROSHEETCODE': 'str',
+    'PLAYERID': 'str'
+  }
+  data = _get_data(uri, dtypes=dtypes, encoding='ISO-8859-1')
+
+  if not data.empty:
+    data = _prep_players_historical(data)
+    _to_db_with_temp(data=data, engine=engine, table_name=DB_CONFIG['players_table'], method='ignore')
+
+def _get_data(uri, max_tries=10, encoding='utf-8', dtypes={}):
   """Get pandas DataFrame from URI
 
   Queries provided URI for a CSV, then returns it as a DataFrame. Exponentially
@@ -90,6 +146,8 @@ def _get_data(uri, max_tries=10, encoding='utf-8'):
     max_tries (int): A maximum number of tries to query the URI. Defaults to 10.
     encoding (string): An encoding string that determines how the URI resource is 
       intepreted. Defaults to 'utf-8'.
+    dtypes (object): An object defining the data types for any columns that need
+      to be manually specified.
   """
 
   successful = False
@@ -99,13 +157,14 @@ def _get_data(uri, max_tries=10, encoding='utf-8'):
 
   while not successful and tries <= max_tries:
       try:
-          data = pd.read_csv(uri, low_memory=False, encoding=encoding)
+          data = pd.read_csv(uri, low_memory=False, encoding=encoding, dtype=dtypes)
           successful = True
       except HTTPError:
           backoff_time = min(backoff_time * 2, 60*60)
           tries += 1
 
   return data
+
 
 def _prep_statcast(data):
   """Prepare a Statcast DataFrame
@@ -121,35 +180,64 @@ def _prep_statcast(data):
   data.drop('pos2_person_id.1', axis=1, inplace=True)
 
   data.rename(columns={
-      'start_speed': 'release_speed',
-      'x0': 'release_pos_x',
-      'z0': 'release_pos_z',
-      'spin_rate': 'spin_rate_deprecated',
-      'break_angle': 'break_angle_deprecated',
-      'break_length': 'break_length_deprecated',
-      'inning_top_bottom': 'inning_topbot',
-      'tfs': 'tfs_deprecated',
-      'tfs_zulu': 'tfs_zulu_deprecated',
-      'catcher': 'pos2_person_id',
-      'hit_speed': 'launch_speed',
-      'hit_angle': 'launch_angle',
-      'px': 'plate_x',
-      'pz': 'plate_z'
+    'start_speed': 'release_speed',
+    'x0': 'release_pos_x',
+    'z0': 'release_pos_z',
+    'spin_rate': 'spin_rate_deprecated',
+    'break_angle': 'break_angle_deprecated',
+    'break_length': 'break_length_deprecated',
+    'inning_top_bottom': 'inning_topbot',
+    'tfs': 'tfs_deprecated',
+    'tfs_zulu': 'tfs_zulu_deprecated',
+    'catcher': 'pos2_person_id',
+    'hit_speed': 'launch_speed',
+    'hit_angle': 'launch_angle',
+    'px': 'plate_x',
+    'pz': 'plate_z'
   }, inplace=True)
 
   return data
 
-def _to_db_with_temp(data, engine, table_name, chunksize=5000):
+
+def _prep_players_historical(data):
+  """Prepare a Historical Player map DataFrame
+
+  Transforms the Historical Player data to suit the `players` table schema.
+  """
+
+  data['mlb_name'] = data['FIRSTNAME'] + ' ' + data['LASTNAME']
+
+  data.rename(columns={
+    'MLBCODE': 'mlb_id',
+    'RETROSHEETCODE': 'retro_id',
+    'PLAYERID': 'bp_id'
+  }, inplace=True)
+
+  data.drop(columns=[
+    'LASTNAME',
+    'FIRSTNAME',
+    'DAVENPORTCODE'
+  ], inplace=True)
+
+  data = data[pd.notnull(data['mlb_id'])]
+
+  return data
+
+
+def _to_db_with_temp(data, engine, table_name, method='replace', chunksize=5000):
   """Insert data in database using a temporary table
 
   Creates a temporary table in the database to write to before committing to
-    the correct table via 'REPLACE INTO' syntax. Prevents duplicate records
+    the correct table. Prevents duplicate records
     in the database.
 
   Args:
     data (DataFrame): A DataFrame to insert into the database.
     engine (engine): A SQLAlchemy engine connected to the database.
     table_name (string): A name of the database table to write to.
+    method (string): A string determining how duplicate records are handled.
+      'replace' forces new duplicate records to overwrite existing ones.
+      'ignore' prevents new duplicate records from overwriting existing ones.
     chunksize (int): A number of records to insert into the database at once.
   """
 
@@ -160,14 +248,20 @@ def _to_db_with_temp(data, engine, table_name, chunksize=5000):
   data.to_sql(name='temp', con=engine, if_exists='append',
               index=False, chunksize=chunksize)
 
-  engine.execute('REPLACE INTO {} SELECT * FROM temp;'.format(table_name))
+  if method == 'replace':
+    engine.execute('REPLACE INTO {} SELECT * FROM temp;'.format(table_name))
+  elif method == 'ignore':
+    engine.execute('INSERT IGNORE INTO {} SELECT * FROM temp;'.format(table_name))
+  
   engine.execute('DROP TABLE temp;')
+
 
 def main():
   """Main"""
   parser = argparse.ArgumentParser()
   parser.add_argument('--no-statcast', help='Do not update the statcast table.', action='store_true')
   parser.add_argument('--no-players', help='Do not update the players table.', action='store_true')
+  parser.add_argument('--historical-players', help='Update the players table with historical IDs.', action='store_true')
   parser.add_argument('--year', help='Enter the year to update. Defaults to current year.', type=int)
   args = parser.parse_args()
 
@@ -184,6 +278,9 @@ def main():
   if not args.no_players:
     print('\nUPDATING PLAYERS\n')
     update_players(engine)
+  if args.historical_players:
+    print('\nUPDATING HISTORICAL PLAYERS\n')
+    update_players_historical(engine)
 
 if __name__ == "__main__":
   main()
